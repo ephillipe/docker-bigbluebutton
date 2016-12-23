5b3fa17df943c0696f152b9bd120c6cf6adeecd3
Erick Almeida <ephillipe@gmail.com>
2016-12-23 16:25:36 -0200

    Funcionando

--- scripts/bbb-start.sh
+++ scripts/bbb-start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # Little helper start script for BigBlueButton in a docker container.
 # Author: Juan Luis Baptiste <juan.baptiste@gmail.com>
+# Maintainer: Erick Almeida <ephillipe@gmail.com>
 
+TOMCAT_VERSION=tomcat7
 DEFAULT_BBB_INSTALL_DEMOS="no"
 
-function get_ip (){
+function get_ip {
     /sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
 }
 
-IP=`get_ip`
+IP=$(get_ip)
+echo "Discovered Server IP: $IP"
 
 if [ ! -z $BBB_INSTALL_DEMOS -a "$BBB_INSTALL_DEMOS" == "yes" ]; then
     echo -e "\e[92mInstalling BigBlueButton demo package...\n\e[0m"
@@ -18,7 +21,7 @@ if [ ! -z $BBB_INSTALL_DEMOS -a "$BBB_INSTALL_DEMOS" == "yes" ]; then
 fi
 
 echo -e "\e[92mStarting BigBlueButton services...\e[0m"
-service redis-server-2.2.4 start
+service redis-server start
 service bbb-openoffice-headless start
 echo -e "\e[92mUpdating BigBlueButton IP address configuration...\e[0m"
 
@@ -33,20 +36,22 @@ fi
 
 #Set new hostname
 bbb-conf --setip $IP
+
 #Replace the IP address on the demo web app, it seems 
 #bbb-conf --setip doesn't do it
 echo -e "\n\e[92mChanging IP address in demo API:\e[0m $IP"
-sed -ri "s/(.*BigBlueButtonURL *= *\").*/\1http:\/\/$IP\/bigbluebutton\/\";/" /var/lib/tomcat6/webapps/demo/bbb_api_conf.jsp
+sed -ri "s/(.*BigBlueButtonURL *= *\").*/\1http:\/\/$IP\/bigbluebutton\/\";/" /var/lib/$TOMCAT_VERSION/webapps/demo/bbb_api_conf.jsp
+
 #It seems that some times bbb-conf --setsecret doesn't set the secret on the demo api conf file.
-sed -ri "s/(.*salt *= *\").*/\1$SERVER_SALT\";/" /var/lib/tomcat6/webapps/demo/bbb_api_conf.jsp
+sed -ri "s/(.*salt *= *\").*/\1$SERVER_SALT\";/" /var/lib/$TOMCAT_VERSION/webapps/demo/bbb_api_conf.jsp
+
 #Set the mobile salt to enable mobile access
 [ ! -z $MOBILE_SALT ] && echo -e "\n\e[92mSetting mobile salt to:\e[0m $MOBILE_SALT"
-[ ! -z $MOBILE_SALT ] && sed -ri "s/(.*mobileSalt *= *\").*/\1$MOBILE_SALT\";/" /var/lib/tomcat6/webapps/demo/mobile_conf.jsp
-
+[ ! -z $MOBILE_SALT ] && sed -ri "s/(.*mobileSalt *= *\").*/\1$MOBILE_SALT\";/" /var/lib/$TOMCAT_VERSION/webapps/demo/mobile_conf.jsp
 [ ! -z $SERVER_SALT ] && echo -e "\n\e[92mSetting Salt to:\e[0m $SERVER_SALT" && bbb-conf --setsecret $SERVER_SALT
 
 #Fix permissions when using a volume container
-chown -R tomcat6:tomcat6 /var/bigbluebutton
+chown -R $TOMCAT_VERSION:$TOMCAT_VERSION /var/bigbluebutton
 
 #For some reason sometimes meetings fail when started from mconf-web
 #until we clean the installation
